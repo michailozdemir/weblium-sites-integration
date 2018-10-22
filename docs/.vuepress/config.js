0aaeb05520974ce71b2711b50f6274b3f5fc2283
Michail Ozdemir <memosh@ukr.net>
2018-10-22 15:50:31 +0300

    add docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  base: '/weblium-sites-integration/',
+  title: 'Weblium Sites Integration',
+  description: 'Кастомная интеграция для блоков и сайтов на Weblium',
+
+themeConfig: {
+    nav: [
+      { text: 'Главная', link: '/' },
+      { text: 'Содержание', link: 'info.md' },
+      { text: 'GitHub', link: 'https://github.com/michailozdemir/weblium-sites-integration/blob/master/README.md' },
+    ] 
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,6 @@
+---
+home: true
+actionText: К содержанию →
+actionLink: /guide/
+footer: MIT Licensed | Copyright © 2018 Michail Ozdemir
+---
